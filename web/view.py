1965abc8952dd270d5e6b7cb26c3fb7479821a82
tcrow <mrdwy.luo@qq.com>
2019-09-23 11:14:44 +0800

    1

--- web/view.py
+++ web/view.py
@@ -1,11 +1,12 @@
 import json
 
 import requests
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
-es_url = 'http://127.0.0.1:9200'
-index = '/meituan/blog/'
+es_url = 'http://10.0.0.39:9200'
+index = '/meituan/blog'
+
 
 def hello(request):
     context = {}
@@ -15,23 +16,35 @@ def hello(request):
 
 
 def show(request):
-    url = es_url + index + request.GET['id']
+    url = es_url + index + '/' + request.GET['id']
     r = requests.get(url)
     return HttpResponse(json.loads(r.content)["_source"]["content"])
 
 
 def search(request):
     url = es_url + index + "/_search"
-    data = {
-        "query": {
+    must = []
+    if len(request.GET['q'].strip()):
+        must.append({
             "match": {
                 "name": request.GET['q']
-            },
+            }
+        })
+    if len(request.GET['c'].strip()):
+        must.append({
             "match_phrase": {
                 "content": request.GET['c']
             }
-        },
-        "size": 10
+        })
+    start = int(request.GET['p']) * 10
+    data = {
+        "size": 10,
+        "from": start,
+        "query": {
+            "bool": {
+                "must": must
+            }
+        }
     }
     headers = {'Accept-Charset': 'utf-8', 'Content-Type': 'application/json'}
     r = requests.get(url, headers=headers, data=json.dumps(data))
@@ -43,4 +56,8 @@ def search(request):
         list.append(blog)
     context = {}
     context['list'] = list
+    context['c'] = request.GET['c']
+    context['q'] = request.GET['q']
+    context['p'] = request.GET['p']
+
     return render(request, 'list.html', context)

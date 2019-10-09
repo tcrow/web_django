c828eeb8a4dc90f647adca99b2d1d0d44801d87f
tcrow <mrdwy.luo@qq.com>
2019-10-09 10:31:54 +0800

    :sparkles: 调试微信接口功能

--- web/wechat_server.py
+++ web/wechat_server.py
@@ -1,29 +1,30 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-
-from wechatpy import parse_message
 import hashlib
 import logging
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from wechatpy import parse_message
+
 # 生成一个以当前文件名为名字的logger实例
 logger = logging.getLogger(__name__)
 
-#公众号token
+# 公众号token
 token = '123'
+
 
 @csrf_exempt
 def index(request):
-
     signature = request.GET['signature']
     echostr = request.GET['echostr']
     timestamp = request.GET['timestamp']
     nonce = request.GET['nonce']
     list = [token, timestamp, nonce]
-    logger.error(list)
     list.sort()
     sha1 = hashlib.sha1()
     map(sha1.update, list)
     hashcode = sha1.hexdigest()
-    print("handle/GET func: hashcode, signature: ", hashcode, signature)
+    logger.error("handle/GET func: signature, echostr, timestamp, nonce: ", signature, echostr, timestamp, nonce)
+    logger.error("handle/GET func: hashcode, signature: ", hashcode, signature)
     if hashcode == signature:
         return HttpResponse(echostr)
     else:

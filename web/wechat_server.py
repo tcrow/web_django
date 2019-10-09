55fb91a3dd0bdb6b7b9b3927a70f50543b534e24
tcrow <mrdwy.luo@qq.com>
2019-10-09 09:11:59 +0800

    :bug: 增加微信接口日志打印功能

--- web/wechat_server.py
+++ web/wechat_server.py
@@ -8,6 +8,7 @@ from wechatpy import parse_message
 def index(request):
     xml = request.body.decode()
     msg = parse_message(xml)
+    print(msg)
     if msg.type == 'link':
         return HttpResponse()
     else:
